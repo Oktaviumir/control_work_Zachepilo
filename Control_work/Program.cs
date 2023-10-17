a2b9bad4aa4649a69a635fa1f61101f975408cbe
Oktavium <oktaviumir@yandex.ru>
2023-10-17 20:48:26 +0300

    Решил задачу

--- Control_work/Program.cs
+++ Control_work/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿string[] array1 = new string[4] { "234", "1567", "-2", "computer science" };
+string[] array2 = new string[array1.Length];
+void Array(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+    return;
+}
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+}
+Array(array1, array2);
+PrintArray(array2);
+
