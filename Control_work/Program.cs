a1adf96ec7f372cb459ef7a9f6af7c8d991279be
Oktavium <oktaviumir@yandex.ru>
2023-10-22 12:14:15 +0300

    Добавил Блок-схему

--- Control_work/Program.cs
+++ Control_work/Program.cs
@@ -1,27 +1,26 @@
-﻿string[] array1 = new string[4] { "234", "1567", "-2", "computer science" };
+﻿Console.Clear();
+
+﻿string[] array1 = new string[] {"Hello", "2", "world", ":-)"};
 string[] array2 = new string[array1.Length];
-void Array(string[] array1, string[] array2)
+void SecondArray(string[] array1, string[] array2)
 {
     int count = 0;
     for (int i = 0; i < array1.Length; i++)
     {
-        if (array1[i].Length <= 3)
+    if(array1[i].Length <= 3)
         {
-            array2[count] = array1[i];
-            count++;
+        array2[count] = array1[i];
+        count++;
         }
     }
-    return;
 }
 void PrintArray(string[] array)
 {
-    Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
         Console.Write($"{array[i]} ");
     }
-    Console.Write("]");
+    Console.WriteLine();
 }
-Array(array1, array2);
+SecondArray(array1, array2);
 PrintArray(array2);
-
